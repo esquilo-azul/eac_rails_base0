a901f690f0764fbffc36b79d0506c7ca519b7cad
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-08-08 16:34:08 -0300

    Rspec installer.

---
+++ lib/generators/eac_rails_base0/install/rspec/templates/spec_rails_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'eac_rails_base0/spec_helper'
