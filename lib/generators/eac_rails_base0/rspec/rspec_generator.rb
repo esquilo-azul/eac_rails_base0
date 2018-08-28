c841e1ab16117134bbf89566d52dee3abe740d83
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-08-28 17:23:34 -0300

    Move generator eac_rails_base0:install:rspec to eac_rails_base0:rspec.

---
+++ lib/generators/eac_rails_base0/rspec/rspec_generator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module EacRailsBase0
+  class RspecGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    def create_initializer_files
+      copy_file 'rspec.rc', '.rspec'
+      copy_file 'spec_rails_helper.rb', 'lib/spec_rails_helper.rb'
+      copy_file 'bin_rspec.rb', 'bin/rspec'
+    end
+  end
+end
