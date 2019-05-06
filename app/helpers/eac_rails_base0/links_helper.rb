89852d6cdb751ac4effda2f43664a85629c5d4e4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-05-05 22:33:44 -0300

    EacRailsBase0::LinksHelper.short_edit_link.

---
+++ app/helpers/eac_rails_base0/links_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+module EacRailsBase0
+  module LinksHelper
+    def short_edit_link(object)
+      value_or_sign(object, '') do |value|
+        link_to '', object_path(value, 'edit'),
+                class: 'edit_link', target: '_blank',
+                title: ::I18n.t('eac_rails_base0.links.edit_object', label: value.to_s)
+      end
+    end
+
+    def object_path(object, action = nil)
+      path = "#{object.class.name.underscore.tr('/', '_')}_url"
+      path = "#{action}_#{path}" if action.present?
+      send(path, object)
+    end
+  end
+end
