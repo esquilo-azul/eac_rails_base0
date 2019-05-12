fe91c56966d6f2f565fd191d2326b9cd00e1d36b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-05-12 19:01:56 -0300

    EacRailsBase0::LinksHelper.short_goto_link.

--- app/helpers/eac_rails_base0/links_helper.rb
+++ app/helpers/eac_rails_base0/links_helper.rb
@@ -9,6 +9,13 @@ module EacRailsBase0
       end
     end
 
+    def short_goto_link(url)
+      value_or_sign(url, '') do |value|
+        link_to '', value, class: 'goto_link', target: '_blank',
+                           title: ::I18n.t('eac_rails_base0.links.goto_url', url: value.to_s)
+      end
+    end
+
     def object_path(object, action = nil)
       path = "#{object.class.name.underscore.tr('/', '_')}_url"
       path = "#{action}_#{path}" if action.present?
