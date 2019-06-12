807f97acabc264aa76bdc73928e013d1f3c64ed0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-06-12 19:42:40 -0300

    EacRailsBase0::LinksHelper.short_detail_link.

--- app/helpers/eac_rails_base0/links_helper.rb
+++ app/helpers/eac_rails_base0/links_helper.rb
@@ -28,17 +28,27 @@ module EacRailsBase0
     end
 
     def short_show_link(object)
-      value_or_sign(object, '') do |value|
-        link_to '', object_path(value),
-                class: 'show_link', target: '_blank',
-                title: ::I18n.t('eac_rails_base0.links.show_object', label: value.to_s)
-      end
+      short_detail_show_link(object, false)
+    end
+
+    def short_detail_link(object)
+      short_detail_show_link(object, true)
     end
 
     def object_path(object, action = nil)
       path = "#{object.class.name.underscore.tr('/', '_')}_url"
       path = "#{action}_#{path}" if action.present?
       send(path, object)
+    end
+
+    private
+
+    def short_detail_show_link(object, detail)
+      value_or_sign(object, detail ? 'detail' : nil) do |value|
+        link_to '', object_path(value),
+                class: 'show_link', target: '_blank',
+                title: ::I18n.t('eac_rails_base0.links.show_object', label: value.to_s)
+      end
     end
   end
 end
