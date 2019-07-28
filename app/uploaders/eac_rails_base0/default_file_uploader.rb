f0d6365a4d3d9e8d56a38fadaf836ab6d141ca94
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-07-28 00:59:07 -0300

    EacRailsBase0::CarrierWave::DefaultFileUploader.

---
+++ app/uploaders/eac_rails_base0/default_file_uploader.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'carrierwave'
+
+module EacRailsBase0
+  class DefaultFileUploader < ::CarrierWave::Uploader::Base
+    if ENV['carrierwave_provider'] == 'AWS'
+      storage :fog
+    else
+      storage :file
+    end
+  end
+end
