d77420efaafe0202012cdd1dcb75488e04ed54cb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-16 12:52:53 -0300

    Generator for Rakefile.

--- lib/generators/eac_rails_base0/testers/testers_generator.rb
+++ lib/generators/eac_rails_base0/testers/testers_generator.rb
@@ -5,6 +5,7 @@ module EacRailsBase0
     source_root File.expand_path('templates', __dir__)
 
     def create_initializer_files
+      copy_file 'Rakefile', 'Rakefile'
       copy_file 'rspec.rc', '.rspec'
       copy_file 'spec_rails_helper.rb', 'lib/spec_rails_helper.rb'
       copy_file 'bin_rspec.rb', 'bin/rspec'
