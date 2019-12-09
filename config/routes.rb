337b8140458c2b8699731130cf638e590a3f1326
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-12-09 12:46:18 -0300

    config/routes: maps EacRailsUtils::Engine.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount ::EacRailsUtils::Engine => '/'
   mount ::EacUsersSupport::Engine => '/'
   mount ::Aranha::Engine => '/aranha'
   mount ::BrRailties::Engine => '/br_railties'
