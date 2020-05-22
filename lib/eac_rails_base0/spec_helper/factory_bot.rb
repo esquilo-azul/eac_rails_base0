862ab7502afb13537443b7082051dba7f2929772
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-22 17:17:11 -0300

    EacRailsBase0::SpecHelper: add engines' directories to FactoryBot.

--- lib/eac_rails_base0/spec_helper/factory_bot.rb
+++ lib/eac_rails_base0/spec_helper/factory_bot.rb
@@ -5,3 +5,13 @@ require 'factory_bot_rails'
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+::FactoryBot.definition_file_paths += ::EacRailsBase0::XEngine.local.flat_map do |engine|
+  %w[factories test/factories spec/factories].map do |subpath|
+    engine.config.root.join(subpath).to_path
+  end
+end
+
+::FactoryBot.find_definitions
+
+Spring.after_fork { FactoryBot.reload } if defined?(Spring)
