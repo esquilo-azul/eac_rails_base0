76656639b9bef2579adaebabb5b3d9cacd0cad42
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-05-22 16:54:04 -0300

    EacRailsBase0::XEngine.

---
+++ lib/eac_rails_base0/x_engine.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'eac_ruby_utils/core_ext'
+require 'find'
+require 'delegate'
+
+module EacRailsBase0
+  # A extended version of [Rails::Engine].
+  class XEngine < SimpleDelegator
+    class << self
+      enable_simple_cache
+
+      def local_root
+        ::Rails.root.join('engines')
+      end
+
+      private
+
+      def all_uncached
+        ::Rails::Engine.subclasses.map { |engine_class| new(engine_class.instance) }
+      end
+
+      def local_uncached
+        all.select(&:local?)
+      end
+    end
+
+    def initialize(engine_instance)
+      super(engine_instance)
+    end
+
+    def local?
+      return false unless self.class.local_root.directory?
+
+      ::Find.find(self.class.local_root.to_path).include?(config.root.to_path)
+    end
+  end
+end
