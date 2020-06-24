86849d8dded056bf265e7b63f3c794ba35f8705d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-06-24 10:20:35 -0300

    EacRailsBase0::Engine: require engines used in "config/routes.rb".

--- lib/eac_rails_base0/engine.rb
+++ lib/eac_rails_base0/engine.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require 'aranha/engine'
+require 'br_railties/engine'
+require 'eac_rails_utils/engine'
+require 'eac_users_support/engine'
+
 module EacRailsBase0
   class Engine < ::Rails::Engine
   end
