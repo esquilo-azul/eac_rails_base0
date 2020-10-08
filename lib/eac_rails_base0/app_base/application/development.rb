88833778146e22fa18ea3a11049445ce6ba3273c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-08 17:25:25 -0300

    EacRailsBase0::AppBase::Application::Development.setup_general.

--- lib/eac_rails_base0/app_base/application/development.rb
+++ lib/eac_rails_base0/app_base/application/development.rb
@@ -8,7 +8,7 @@ module EacRailsBase0App
       common_concern do
         next unless ::Rails.env.development?
 
-        setup('assets', 'log', 'letter_opener')
+        setup('assets', 'general', 'log', 'letter_opener')
       end
 
       module ClassMethods
@@ -16,6 +16,16 @@ module EacRailsBase0App
           config.assets.debug = true
           config.assets.digest = true
           config.assets.raise_runtime_errors = true
+        end
+
+        def setup_general
+          config.cache_classes = false
+          config.eager_load = false
+          config.consider_all_requests_local = true
+          config.action_controller.perform_caching = false
+          config.action_mailer.raise_delivery_errors = false
+
+          config.active_record.migration_error = :page_load
         end
 
         def setup_letter_opener
