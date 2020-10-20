23032a65be193881c0e524e7aa31352b8e2dacb1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-20 10:39:30 -0300

    config/initializers/action_mailer.rb: do not apply if on test environment.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-Rails.application.configure do
-  config.action_mailer.smtp_settings = {
-    address: ENV['action_mailer_smtp_address'],
-    port: ENV['action_mailer_smtp_port'],
-    domain: ENV['action_mailer_smtp_domain'],
-    user_name: ENV['action_mailer_smtp_username'],
-    password: ENV['action_mailer_smtp_password'],
-    authentication: ENV['action_mailer_smtp_authentication'],
-    enable_starttls_auto:
-      ::EacRailsBase0::BooleanValue.to_b(ENV['action_mailer_smtp_enable_starttls_auto'])
-  }
-  %i[host port].each do |option|
-    value = ENV["action_mailer_default_url_#{option}"]
-    if value.present?
-      config.action_mailer.default_url_options ||= {}
-      config.action_mailer.default_url_options[option] = value
+unless ::Rails.env.test?
+  Rails.application.configure do
+    config.action_mailer.smtp_settings = {
+      address: ENV['action_mailer_smtp_address'],
+      port: ENV['action_mailer_smtp_port'],
+      domain: ENV['action_mailer_smtp_domain'],
+      user_name: ENV['action_mailer_smtp_username'],
+      password: ENV['action_mailer_smtp_password'],
+      authentication: ENV['action_mailer_smtp_authentication'],
+      enable_starttls_auto:
+        ::EacRailsBase0::BooleanValue.to_b(ENV['action_mailer_smtp_enable_starttls_auto'])
+    }
+    %i[host port].each do |option|
+      value = ENV["action_mailer_default_url_#{option}"]
+      if value.present?
+        config.action_mailer.default_url_options ||= {}
+        config.action_mailer.default_url_options[option] = value
+      end
     end
+    config.action_mailer.default_options ||= {}
+    config.action_mailer.default_options[:from] = ENV['action_mailer_default_options_from']
+    config.action_mailer.default_options[:reply_to] = ENV['action_mailer_default_options_reply_to']
   end
-  config.action_mailer.default_options ||= {}
-  config.action_mailer.default_options[:from] = ENV['action_mailer_default_options_from']
-  config.action_mailer.default_options[:reply_to] = ENV['action_mailer_default_options_reply_to']
 end
