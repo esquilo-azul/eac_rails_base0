0222b533f130f2f386fe74cc9d78c5b11d57b72a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-20 11:55:09 -0300

    EacRailsBase0::SendTestMailer: extend EacRailsBase0::ApplicationMailer.

--- app/mailers/eac_rails_base0/send_test_mailer.rb
+++ app/mailers/eac_rails_base0/send_test_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module EacRailsBase0
-  class SendTestMailer < ::ApplicationMailer
+  class SendTestMailer < ::EacRailsBase0::ApplicationMailer
     def main
       @subject = "#{t('eac_rails_base0.mailer.send_test')} [#{::Time.zone.now}]"
       mail(to: params.fetch(:address), subject: @subject)
