c75d11aa8672a393c8b155b69161e26be7e95df8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-19 22:02:17 -0300

    EacRailsBase0::AppBase::Application::Test.setup_action_mailer.

--- lib/eac_rails_base0/app_base/application/test.rb
+++ lib/eac_rails_base0/app_base/application/test.rb
@@ -5,16 +5,26 @@ require 'eac_ruby_utils/core_ext'
 module EacRailsBase0App
   class Application < Rails::Application
     module Test
+      ACTION_MAILER_CONFIGS = {
+        delivery_method: :test
+      }.freeze
+
       common_concern do
         next unless ::Rails.env.test?
 
-        setup('action_controller', 'general', 'log', 'public_file_server')
+        setup('action_controller', 'action_mailer', 'general', 'log', 'public_file_server')
       end
 
       module ClassMethods
         def setup_action_controller
           config.action_controller.perform_caching = false
           config.action_controller.allow_forgery_protection = false
+        end
+
+        def setup_action_mailer
+          ACTION_MAILER_CONFIGS.each do |key, value|
+            config.action_mailer.send("#{key}=", value)
+          end
         end
 
         def setup_general
@@ -24,7 +34,6 @@ module EacRailsBase0App
           config.consider_all_requests_local = true
           config.action_controller.perform_caching = false
           config.action_dispatch.show_exceptions = false
-          config.action_mailer.delivery_method = :test
         end
 
         def setup_log
