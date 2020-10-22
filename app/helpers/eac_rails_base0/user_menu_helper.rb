d611875fd0afd9fe56619527bedd4c8f2a644465
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-22 17:07:28 -0300

    EacRailsBase0::UserMenuHelper.base0_user_menu_entries: entry to change password.

--- app/helpers/eac_rails_base0/user_menu_helper.rb
+++ app/helpers/eac_rails_base0/user_menu_helper.rb
@@ -5,7 +5,9 @@ module EacRailsBase0
     def base0_user_menu_entries
       r = {}
       if current_user
-        r[current_user.to_s] = [main_app.root_path]
+        r[current_user.to_s] = {
+          'Alterar senha' => [eac_users_support.edit_user_registration_path]
+        }
         r['Sair'] = [eac_users_support.destroy_user_session_path, link_method: :delete]
       else
         r['Entrar'] = [eac_users_support.new_user_session_path]
