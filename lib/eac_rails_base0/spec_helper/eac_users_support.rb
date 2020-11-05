6159604a154c53f52e24b3e2dfa037b8180e3e03
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2020-11-05 15:15:05 -0300

    RSpec.shared_context('when user is non admin').

--- lib/eac_rails_base0/spec_helper/eac_users_support.rb
+++ lib/eac_rails_base0/spec_helper/eac_users_support.rb
@@ -17,10 +17,34 @@ FactoryBot.define do
     password { 'mysecret' }
     confirmed_at { ::Time.zone.now }
   end
+
+  factory :non_admin_user, class: ::EacUsersSupport::User do
+    email { 'nonadmin@example.com' }
+    password { 'mysecret' }
+    confirmed_at { ::Time.zone.now }
+  end
 end
 
 RSpec.shared_context 'when user is admin', shared_context: :metadata do
   let(:user) { ::FactoryBot.create(:admin_user) }
+
+  before do
+    visit '/users/sign_in'
+    fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.email'),
+            with: user.email
+    fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.password'),
+            with: user.password
+    click_button ::I18n.translate!('devise.log_in')
+  end
+
+  it 'user should be logged' do
+    expect(page).to have_content user.email
+    expect(::EacUsersSupport::User.current_user).not_to be_nil
+  end
+end
+
+RSpec.shared_context 'when user is non admin', shared_context: :metadata do
+  let(:user) { ::FactoryBot.create(:non_admin_user) }
 
   before do
     visit '/users/sign_in'
