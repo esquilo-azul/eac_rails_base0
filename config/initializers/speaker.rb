9fe9ad65b890e4d585914ccb5cf5f164c07e5aba
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-28 12:23:37 -0300

    config/initializers/speaker: setup EacRubyUtils::Speaker.context.

---
+++ config/initializers/speaker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'eac_cli/speaker'
+require 'eac_ruby_utils/speaker'
+
+unless ::EacRubyUtils::Speaker.context.optional_current
+  ::EacRubyUtils::Speaker.context.push(::EacCli::Speaker.new)
+end
