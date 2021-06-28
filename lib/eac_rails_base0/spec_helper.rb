fba1742ff782dbbaf9746773d1911809a9f9e482
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-28 14:05:21 -0300

    eac_rails_base0/spec_helper.rb: setup EacRubyGemSupport::Rspec.

--- lib/eac_rails_base0/spec_helper.rb
+++ lib/eac_rails_base0/spec_helper.rb
@@ -22,6 +22,9 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  require 'eac_ruby_gem_support/rspec'
+  ::EacRubyGemSupport::Rspec.setup(::File.expand_path('..', __dir__), config)
 end
 
 require 'eac_ruby_utils/require_sub'
