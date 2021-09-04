237fdee458b4be6e7ba48444b2bd126169e4e9af
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-04 11:15:10 -0300

    config/initializers/active_scaffold.rb: adapt to "active_scaffold" '3.6.2'.

--- config/initializers/active_scaffold.rb
+++ config/initializers/active_scaffold.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-ActiveScaffold.set_defaults do |conf|
+::ActiveScaffold.defaults do |conf|
   conf.list.per_page = 100
   conf.security.default_permission = false
 end
