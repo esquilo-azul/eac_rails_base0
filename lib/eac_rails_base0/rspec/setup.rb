665768c0bf48233e457d56e573725bfd8675e5bb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-03 21:42:52 -0300

    eac_rails_base0/rspec/shared_contexts/when_user_is_admin.rb: extract from EacRailsBase0::Rspec.setup_admin_example.

--- lib/eac_rails_base0/rspec/setup.rb
+++ lib/eac_rails_base0/rspec/setup.rb
@@ -46,24 +46,8 @@ module EacRailsBase0
         end
       end
 
-      def setup_admin_example # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-        ::RSpec.shared_context 'when user is admin', shared_context: :metadata do
-          let(:user) { ::FactoryBot.create(:admin_user) }
-
-          before do
-            visit '/users/sign_in'
-            fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.email'),
-                    with: user.email
-            fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.password'),
-                    with: user.password
-            click_button ::I18n.translate!('devise.log_in')
-          end
-
-          it 'user should be logged' do
-            expect(page).to have_content user.email
-            expect(::EacUsersSupport::User.current_user).not_to be_nil
-          end
-        end
+      def setup_admin_example
+        require 'eac_rails_base0/rspec/shared_contexts/when_user_is_admin'
       end
 
       def setup_non_admin_example # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
---
+++ lib/eac_rails_base0/rspec/shared_contexts/when_user_is_admin.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+::RSpec.shared_context 'when user is admin', shared_context: :metadata do
+  let(:user) { ::FactoryBot.create(:admin_user) }
+
+  before do
+    visit '/users/sign_in'
+    fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.email'),
+            with: user.email
+    fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.password'),
+            with: user.password
+    click_button ::I18n.translate!('devise.log_in')
+  end
+
+  it 'user should be logged' do
+    expect(page).to have_content user.email
+    expect(::EacUsersSupport::User.current_user).not_to be_nil
+  end
+end
