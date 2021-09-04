1262bf8afc99f38667dfd3911c2b70f6650403b8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-09-03 21:43:28 -0300

    eac_rails_base0/rspec/shared_contexts/when_user_is_non_admin.rb: extract from EacRailsBase0::Rspec.setup_non_admin_example.

--- lib/eac_rails_base0/rspec/setup.rb
+++ lib/eac_rails_base0/rspec/setup.rb
@@ -50,24 +50,8 @@ module EacRailsBase0
         require 'eac_rails_base0/rspec/shared_contexts/when_user_is_admin'
       end
 
-      def setup_non_admin_example # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-        ::RSpec.shared_context 'when user is non admin', shared_context: :metadata do
-          let(:user) { ::FactoryBot.create(:non_admin_user) }
-
-          before do
-            visit '/users/sign_in'
-            fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.email'),
-                    with: user.email
-            fill_in ::I18n.translate!('activerecord.attributes.eac_users_support/user.password'),
-                    with: user.password
-            click_button ::I18n.translate!('devise.log_in')
-          end
-
-          it 'user should be logged' do
-            expect(page).to have_content user.email
-            expect(::EacUsersSupport::User.current_user).not_to be_nil
-          end
-        end
+      def setup_non_admin_example
+        require 'eac_rails_base0/rspec/shared_contexts/when_user_is_non_admin'
       end
 
       def setup_anonymous_example # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
