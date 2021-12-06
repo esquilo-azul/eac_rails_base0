49d20dab3e4361d07839e4fd011ddc52f18f615f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-12-06 09:43:02 -0300

    EacRailsBase0::Gems: remove unused.

--- lib/eac_rails_base0.rb
+++ lib/eac_rails_base0.rb
@@ -17,7 +17,6 @@ require 'rails-i18n'
 module EacRailsBase0
 end
 
-require_dependency 'eac_rails_base0/gems'
 require_dependency 'eac_rails_base0/app_base/ability_mapping'
 require_dependency 'eac_rails_base0/app_base/ability'
 require_dependency 'eac_rails_base0/patches/url_for_patch'
