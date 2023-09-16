ed2cd591c6eb7b5775cf1b98f0284c00111516e5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-09-16 19:42:04 -0300

    EacRailsBase0::MainMenuHelper::ADMIN_ENTRIES: remove.

--- app/helpers/eac_rails_base0/main_menu_helper.rb
+++ app/helpers/eac_rails_base0/main_menu_helper.rb
@@ -3,7 +3,6 @@
 module EacRailsBase0
   module MainMenuHelper
     APP_MAIN_MENU_ENTRIES_METHOD = 'app_main_menu_entries'
-    ADMIN_ENTRIES = %w[].freeze
 
     def base0_app_main_menu_entries
       if respond_to?(APP_MAIN_MENU_ENTRIES_METHOD)
@@ -20,14 +19,6 @@ module EacRailsBase0
     end
 
     def base0_app_main_menu_admin_entries
-      ADMIN_ENTRIES.map do |identifier|
-        [::I18n.t("eac_rails_base0.main_menu.admin.#{identifier}"),
-         send("#{identifier}_main_menu_admin_entries")]
-      end.to_h.merge(base0_app_main_menu_admin_entries_from_application)
-    end
-
-    # @return [Hash]
-    def base0_app_main_menu_admin_entries_from_application
       ::Rails.application.root_menu.sub(:admin).to_dropdown_menu_entries(self)
     end
   end
