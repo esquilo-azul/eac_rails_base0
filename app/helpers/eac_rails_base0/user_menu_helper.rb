7a4a3374612607d1148494f86916e2e08485adb0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-11-02 20:56:14 -0300

    EacRailsBase0::UserMenuHelper.base0_user_menu_entries_logged: extract from .base0_user_menu_entries.

--- app/helpers/eac_rails_base0/user_menu_helper.rb
+++ app/helpers/eac_rails_base0/user_menu_helper.rb
@@ -2,17 +2,26 @@
 
 module EacRailsBase0
   module UserMenuHelper
+    # @return [Hash]
     def base0_user_menu_entries
       r = {}
-      if current_user
-        r[current_user.to_s] = {
-          'Alterar senha' => [eac_users_support.edit_user_registration_path]
-        }
-        r[t('devise.log_out')] = [eac_users_support.destroy_user_session_path, link_method: :delete]
-      else
-        r[t('devise.log_in')] = [eac_users_support.new_user_session_path]
-      end
+      return base0_user_menu_entries_logged if current_user
+
+      r[t('devise.log_in')] = [eac_users_support.new_user_session_path]
       r
+    end
+
+    private
+
+    # @return [Hash]
+    def base0_user_menu_entries_logged
+      {
+        current_user.to_s => {
+          'Alterar senha' => [eac_users_support.edit_user_registration_path]
+        },
+        t('devise.log_out') => [eac_users_support.destroy_user_session_path,
+                                { link_method: :delete }]
+      }
     end
   end
 end
