2b2adc763f5a23f21874f8cd9bfee71ec6c5067c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-01-05 15:00:49 -0300

    .rubocop.yml#Style/RedundantFileExtensionInRequire: remove.

--- spec/support/rails_app_append/config/application.rb
+++ spec/support/rails_app_append/config/application.rb
@@ -2,4 +2,4 @@
 
 APP_PATH = File.dirname(__dir__) unless defined?(APP_PATH)
 require File.expand_path('boot', __dir__)
-require 'eac_rails_base0/app_base/application.rb'
+require 'eac_rails_base0/app_base/application'
