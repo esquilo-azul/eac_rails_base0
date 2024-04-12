c23dc8f5d957e02d420c35857c854823b3ee97b0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-12 01:17:54 -0300

    EacRailsBase0::Engine: fix missing requires.

--- lib/eac_rails_base0/engine.rb
+++ lib/eac_rails_base0/engine.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 require 'aranha/rails/engine'
+require 'bootstrap-sass'
 require 'br_railties/engine'
+require 'can_can_dry/engine'
 require 'eac_active_scaffold/engine'
 require 'eac_rails_delayed_job/engine'
 require 'eac_rails_remotes/engine'
 require 'eac_rails_utils/engine'
 require 'eac_users_support/engine'
+require 'rails-i18n'
+require 'jquery-rails'
+require 'jquery-ui-rails'
+require 'tasks_scheduler/engine'
+require 'turbolinks'
 
 module EacRailsBase0
   class Engine < ::Rails::Engine
