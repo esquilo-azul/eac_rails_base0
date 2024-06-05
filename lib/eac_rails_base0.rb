24c5c03113d5c5dd86e3e6c8ecdae20cea26cc2c
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2024-06-05 12:06:19 -0300

    EacRailsBase0::Engine: extract requires from EacRailsBase0.

--- lib/eac_rails_base0.rb
+++ lib/eac_rails_base0.rb
@@ -1,23 +1,4 @@
 # frozen_string_literal: true
 
-require 'sass-rails'
-require 'jquery-rails'
-require 'jquery-ui-rails'
-require 'turbolinks'
-require 'bootstrap-sass'
-require 'can_can_dry'
-require 'eac_rails_utils'
-require 'eac_ruby_utils/core_ext'
-require 'eac_users_support'
-require 'eac_rails_base0/engine'
-require 'tasks_scheduler'
-require 'br_railties'
-require 'rails-i18n'
-
 module EacRailsBase0
 end
-
-require_dependency 'eac_rails_base0/app_base/ability_mapping'
-require_dependency 'eac_rails_base0/app_base/ability'
-require_dependency 'eac_rails_base0/patches/url_for_patch'
-require_dependency 'eac_rails_base0/x_engine'
--- lib/eac_rails_base0/engine.rb
+++ lib/eac_rails_base0/engine.rb
@@ -1,20 +1,34 @@
 # frozen_string_literal: true
 
 require 'aranha/rails/engine'
-require 'bootstrap-sass'
 require 'br_railties/engine'
+require 'bootstrap-sass'
+require 'can_can_dry'
 require 'can_can_dry/engine'
 require 'eac_active_scaffold/engine'
+require 'eac_rails_base0/engine'
 require 'eac_rails_delayed_job/engine'
 require 'eac_rails_remotes/engine'
 require 'eac_rails_utils/engine'
 require 'eac_rails_utils/engine_helper'
 require 'eac_users_support/engine'
-require 'rails-i18n'
 require 'jquery-rails'
 require 'jquery-ui-rails'
+require 'rails-i18n'
+require 'sass-rails'
 require 'tasks_scheduler/engine'
 require 'turbolinks'
+
+# require 'eac_rails_utils'
+# require 'eac_ruby_utils/core_ext'
+# require 'eac_users_support'
+# require 'tasks_scheduler'
+# require 'br_railties'
+
+require_dependency 'eac_rails_base0/app_base/ability_mapping'
+require_dependency 'eac_rails_base0/app_base/ability'
+require_dependency 'eac_rails_base0/patches/url_for_patch'
+require_dependency 'eac_rails_base0/x_engine'
 
 module EacRailsBase0
   class Engine < ::Rails::Engine
