64a40d87b430df087a76f4dfa9a9601d6b2c48b1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-25 13:01:08 -0300

    EacRailsBase0::AbilitiesGenerator: remove.

--- lib/generators/eac_rails_base0/abilities/templates/ability.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Ability < EacRailsBase0::AppBase::Ability
-  def initialize(*args)
-    super
-    can :read, :public
-  end
-end
--- lib/generators/eac_rails_base0/abilities/templates/ability_mapping.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AbilityMapping < EacRailsBase0::AppBase::AbilityMapping
-  def initialize(*args)
-    super
-    map_action 'Welcome', 'index', :read, :public
-  end
-end
